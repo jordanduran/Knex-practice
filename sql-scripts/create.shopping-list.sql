919b8a9471484dda764e5d5de56c2f37af47b81e
Jordan Duran <jordanduranm@gmail.com>
2019-09-18 20:21:40 -0400

    updated sql

--- sql-scripts/create.shopping-list.sql
+++ sql-scripts/create.shopping-list.sql
@@ -6,3 +6,12 @@ CREATE TYPE grocery AS ENUM (
     'Lunch',
     'Breakfast'
 );
+
+CREATE TABLE IF NOT EXISTS shopping_list (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    price decimal(10, 2) NOT NULL,
+    date_added TIMESTAMP DEFAULT now() NOT NULL,
+    checked BOOLEAN DEFAULT false,
+    category grocery NOT NULL
+);
